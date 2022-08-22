78796645c138d053449fcb9eb1bbedfd37e59595
Lera <valeriiakolesnikova21@gmail.com>
2022-08-22 10:55:37 +0300

    task1 added

---
+++ task1/Program.cs
@@ -0,0 +1,20 @@
+﻿//создание переменных
+double a;
+string input;
+
+double result;
+//ввод данных
+input = Console.ReadLine();
+// c# convert string to int
+a = Double.Parse(input);
+// int -> string
+// string -> double
+// int -> string
+// string -> int
+
+// возвести в квадрат
+result = a * a;
+
+//выдать квадрат
+Console.WriteLine("Result square a is:" + result);
+Console.Write(a * a);
