fd77f85f7db3fa794f5995c7dd50ef7486acd66c
Lera <valeriiakolesnikova21@gmail.com>
2022-08-26 08:41:33 +0300

    seminar 3 added

---
+++ 3/task6/Program.cs
@@ -0,0 +1,19 @@
+﻿int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("1");
+}
+else if (x > 0 && y < 0)
+{
+    Console.WriteLine("4");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("2");
+}
+else 
+{
+    Console.WriteLine("3");
+}
---
+++ 3/task7/Program.cs
@@ -0,0 +1,21 @@
+﻿//1. Create array
+int N = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[N];// 10, 100, 1000
+
+//2. Заполнение массива
+int index = 0;
+while (index < N)
+{
+    array[index] = new Random().Next(1, 11);
+    Console.WriteLine(array[index] + " ");
+    index++;
+}
+
+//Average (Mean)
+double sum = 0;
+for (int i = 0; i < N; i++)
+{
+    sum += array[i];
+}
+double mean = sum / N;
+Console.WriteLine(mean);
