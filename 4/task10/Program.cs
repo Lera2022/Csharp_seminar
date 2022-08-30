d11a5d373e3279d9e5dd16e19ce72f409db485f0
Lera <valeriiakolesnikova21@gmail.com>
2022-08-30 11:12:27 +0300

    Seminar 4 added

---
+++ 4/task10/Program.cs
@@ -0,0 +1,32 @@
+﻿// Напишите программу, которая принимает на вход целое число и выдаёт количество цифр в числе, которые являются чётными (больше среднего арифметического среди всех цифр этого числа (1256 - 2 (ср. арифм. 3.5); 45234 - 3 (ср. арифм. 3.6)); указание: - среднее арифметическое среди цифр числа вычислить в отдельной функции, количество цифр в числе больших заданного вычислить в отдельной функции).
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int countOfDigits = 0;
+
+double Avg(int num)
+{
+    double avg = 0;
+    double sum = 0;
+    int tmp = num;
+    while (tmp > 0)
+    {
+        int digit = tmp % 10;
+        sum = sum + digit;
+        countOfDigits++;
+        tmp = tmp / 10;
+    }
+    avg = sum / countOfDigits;
+    return avg;
+}
+double avg = Avg(num);
+Console.WriteLine(avg);
+countOfDigits = 0;
+while (num > 0)
+{
+    int digit = num % 10;
+    if (digit > avg) countOfDigits++;
+    num = num / 10;
+}
+Console.WriteLine(countOfDigits);
---
+++ 4/task9/Program.cs
@@ -0,0 +1,23 @@
+﻿// 1. Input num
+int num = Convert.ToInt32(Console.ReadLine());
+// 2. Get number length in digits
+int countOfDigits = 0;
+int tmp = num;
+while (tmp > 0)
+{
+    tmp = tmp / 10;
+    countOfDigits++;
+}
+// 3. Create array with required size
+int [] digits;
+digits = new int [countOfDigits];
+
+// 4. Fill array digits
+int i = 0;
+while (num > 0)
+{
+    digits[i] = num % 10;
+    num = num / 10;
+    Console.Write(digits[i]);
+    i++;
+}
