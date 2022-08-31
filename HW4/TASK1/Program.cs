7d910d4943a7495b639cb07bc0b3b984c77d93f2
Lera <valeriiakolesnikova21@gmail.com>
2022-08-31 11:21:17 +0300

    HW 4 added

---
+++ HW4/TASK1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. Не использовать функцию Math.Pow()
+// 3, 5 -> 243
+// 2, 4 -> 16
+
+Console.Write("Пожалуйста, введите число А: ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Пожалуйста, введите число В: ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write(A + ", " + B + " -> ");
+
+int i = 0;
+int pow = 1;
+
+while (i < B)
+{
+    pow = pow * A;
+    i++;
+}
+
+Console.Write(pow);
---
+++ HW4/TASK3/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 3:*(Дополнительная, не обязательная задача): Назовём число «интересным» если его произведение цифр делится на их сумму. Напишите программу, которая заполняет массив на N «интересных» случайных целых чисел. (Каждый эл-т массива должен быть сгенерирован случайно)
+
+Console.Write("Пожалуйста, введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(N + ":");
+
+int Sum(int tmp)
+{
+    int sum = 0;
+    while (tmp > 0)
+    {
+        int digit = tmp % 10;
+        sum = sum + digit;
+        tmp = tmp / 10;
+    }
+    return sum;
+}
+
+int Prd(int tmp)
+{
+    int prd = 1;
+    while (tmp > 0)
+    {
+        int digit = tmp % 10;
+        if (digit != 0) //исключила из произведения цифру "0", т.к. получались не интересные ответы)
+        {
+            prd = prd * digit;
+        }
+        tmp = tmp / 10;
+    }
+    return prd;
+}
+
+int[] array = new int[N];
+
+int tmp = 0;
+int index = 0;
+while (index < N)
+{
+    tmp = new Random().Next(1, 2147483647);
+    int sum = Sum(tmp);
+    int prd = Prd(tmp);
+    if (prd % sum == 0)
+    {
+        array[index] = tmp;
+        Console.Write(array[index] + " ");
+        Console.Write("(sum = " + sum + "; ");
+        Console.WriteLine("prd = " + prd + ")");
+        index++;    
+    }
+}
