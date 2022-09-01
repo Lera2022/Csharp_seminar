3d17951566ce298263ace226a7a8c8a52bc2ae51
Lera <valeriiakolesnikova21@gmail.com>
2022-09-01 22:23:28 +0300

    seminar 5  added

---
+++ 5/task11/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9; 9]. Найдите сумму положительных и отрицательных элементов массива.
+int N = 12;
+int[] array = new int[N];
+for (int i = 0; i < N; i++)
+{
+    array[i] = new Random().Next(-9, 10);
+    Console.Write(array[i] + ", ");
+}
+int negSum = 0;
+int posSum = 0;
+for(int i = 0; i<N; i++)
+{
+    if(array[i] < 0)
+    {
+        negSum = negSum + array[i];
+    }
+    else
+    {
+        posSum = posSum + array[i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine(negSum);
+Console.WriteLine(posSum);
---
+++ 5/task12/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задайте произвольный целочисленный массив. Напишите программу, которая определяет, присутствует ли заданное пользователем число в массиве
+int N = 10;
+int[] array = new int[N];
+for (int i = 0; i < N; i++)
+{
+    array[i] = new Random().Next();
+    Console.Write(array[i] + ", ");
+}
+Console.WriteLine();
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void Find(int num)
+{
+    for (int i = 0; i < N; i++)
+    {
+        if (array[i] == num)
+        {
+            Console.WriteLine("да");
+        }
+    }
+
+}
+Find(num);
