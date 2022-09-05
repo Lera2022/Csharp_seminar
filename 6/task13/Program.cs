1159f8bae03130fff7f1bbcc8b76320020afe451
Lera <valeriiakolesnikova21@gmail.com>
2022-09-05 22:39:10 +0300

    seminar 6  added

---
+++ 6/task13/Program.cs
@@ -0,0 +1,18 @@
+﻿int get_factorial(int x)
+{
+    Console.WriteLine(x);
+    if (x == 1)
+    {
+        Console.WriteLine("IF");
+        return 1;
+    }
+    else 
+    {
+        int tmp = get_factorial(x - 1);
+        Console.WriteLine("reverse: " + tmp);
+        return x * tmp;
+    }
+}
+
+int x = get_factorial(4);
+Console.WriteLine(x);
