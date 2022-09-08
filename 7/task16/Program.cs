701dc878f256866d12f007b028125f4111db64d0
Lera <valeriiakolesnikova21@gmail.com>
2022-09-08 22:01:42 +0300

    seminar 7  added

---
+++ 7/task16/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте двумерный массив размером М
+
+int strsCount = Convert.ToInt32(Console.ReadLine());
+int columnsCount = Convert.ToInt32(Console.ReadLine());
+
+//1 allocate memory for array
+int[,] twoDimArray = new int[strsCount, columnsCount];
+
+//2
+Random rnd = new Random();
+for (int i = 0; i < strsCount; i++)
+{
+    for (int j = 0; j < columnsCount; j++)
+    {
+        twoDimArray[i, j] = rnd.Next(1,100);
+        Console.Write(twoDimArray[i, j] + " ");
+    }
+    Console.WriteLine();
+}
---
+++ 7/task17/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте двумерный массив. Найдите элементы, у которых оба индекса чётные, и замените эти элементы на их квадраты.
+
+int M = Convert.ToInt32(Console.ReadLine());
+int N = Convert.ToInt32(Console.ReadLine());
+int[,] nums = new int[M, N];
+Random ran = new Random();
+for (int i = 0; i < M; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        nums[i,j] = ran.Next(1, 10);
+        Console.Write(nums[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+for (int i = 0; i < M; i++)
+{
+    for (int j = 0; j < N; j++)
+    {
+        if (i % 2 == 0 && j % 2 ==0)
+        {
+            nums[i,j] = nums[i,j] * nums[i,j];
+        }
+        Console.Write(nums[i, j] + " ");
+    }
+    Console.WriteLine();
+}
